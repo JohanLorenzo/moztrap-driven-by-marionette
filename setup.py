5140b1ae1ae31c1e2571c14267d107cb1fa0b831
Johan Lorenzo <jlorenzo@mozilla.com>
2015-07-03 15:42:00 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='moztrap-driven-by-marionette',
+    install_requires=[
+       'pyaml',
+       'marionette_client'
+    ],
+    version='',
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
